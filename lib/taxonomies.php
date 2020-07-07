86c9ec00d0054d872e938ca87884b4b394977748
mikeyrayvon <mikey@interglobal.vision>
2020-07-07 22:59:29 +0200

    add album custom tax

--- lib/taxonomies.php
+++ lib/taxonomies.php
@@ -1,65 +1,88 @@
 <?php
 // hook into the init action and call create_book_taxonomies when it fires
-add_action( 'init', 'create_project_taxonomies', 0 );
+add_action( 'init', 'create_album_taxonomies', 0 );
 
 // create two taxonomies, types and themes for the post type "book"
-function create_project_taxonomies() {
+function create_album_taxonomies() {
 	// Add new taxonomy, make it hierarchical (like categories)
 	$labels = array(
-		'name'              => _x( 'Types', 'taxonomy general name', 'igv' ),
-		'singular_name'     => _x( 'Type', 'taxonomy singular name', 'igv' ),
-		'search_items'      => __( 'Search Types', 'igv' ),
-		'all_items'         => __( 'All Types', 'igv' ),
-		'parent_item'       => __( 'Parent Type', 'igv' ),
-		'parent_item_colon' => __( 'Parent Type:', 'igv' ),
-		'edit_item'         => __( 'Edit Type', 'igv' ),
-		'update_item'       => __( 'Update Type', 'igv' ),
-		'add_new_item'      => __( 'Add New Type', 'igv' ),
-		'new_item_name'     => __( 'New Type Name', 'igv' ),
-		'menu_name'         => __( 'Type', 'igv' ),
+		'name'              => _x( 'Artists', 'taxonomy general name', 'igv' ),
+		'singular_name'     => _x( 'Artist', 'taxonomy singular name', 'igv' ),
+		'search_items'      => __( 'Search Artists', 'igv' ),
+		'all_items'         => __( 'All Artists', 'igv' ),
+		'parent_item'       => __( 'Parent Artist', 'igv' ),
+		'parent_item_colon' => __( 'Parent Artist:', 'igv' ),
+		'edit_item'         => __( 'Edit Artist', 'igv' ),
+		'update_item'       => __( 'Update Artist', 'igv' ),
+		'add_new_item'      => __( 'Add New Artist', 'igv' ),
+		'new_item_name'     => __( 'New Artist Name', 'igv' ),
+		'menu_name'         => __( 'Artists', 'igv' ),
 	);
 
 	$args = array(
-		'hierarchical'      => true,
+		'hierarchical'      => false,
 		'labels'            => $labels,
 		'show_ui'           => true,
 		'show_admin_column' => true,
 		'query_var'         => true,
-		'rewrite'           => array( 'slug' => 'type' ),
+		'rewrite'           => array( 'slug' => 'artist' ),
+    'show_in_rest'      => true,
 	);
 
-	register_taxonomy( 'type', array( 'project' ), $args );
+	register_taxonomy( 'artist', array( 'album' ), $args );
 
-	// Add new taxonomy, NOT hierarchical (like tags)
+  // Add new taxonomy, make it hierarchical (like categories)
 	$labels = array(
-		'name'                       => _x( 'Themes', 'taxonomy general name', 'textdomain' ),
-		'singular_name'              => _x( 'Theme', 'taxonomy singular name', 'textdomain' ),
-		'search_items'               => __( 'Search Themes', 'textdomain' ),
-		'popular_items'              => __( 'Popular Themes', 'textdomain' ),
-		'all_items'                  => __( 'All Themes', 'textdomain' ),
-		'parent_item'                => null,
-		'parent_item_colon'          => null,
-		'edit_item'                  => __( 'Edit Theme', 'textdomain' ),
-		'update_item'                => __( 'Update Theme', 'textdomain' ),
-		'add_new_item'               => __( 'Add New Theme', 'textdomain' ),
-		'new_item_name'              => __( 'New Theme Name', 'textdomain' ),
-		'separate_items_with_commas' => __( 'Separate themes with commas', 'textdomain' ),
-		'add_or_remove_items'        => __( 'Add or remove themes', 'textdomain' ),
-		'choose_from_most_used'      => __( 'Choose from the most used themes', 'textdomain' ),
-		'not_found'                  => __( 'No themes found.', 'textdomain' ),
-		'menu_name'                  => __( 'Themes', 'textdomain' ),
+		'name'              => _x( 'Styles', 'taxonomy general name', 'igv' ),
+		'singular_name'     => _x( 'Style', 'taxonomy singular name', 'igv' ),
+		'search_items'      => __( 'Search Styles', 'igv' ),
+		'all_items'         => __( 'All Styles', 'igv' ),
+		'parent_item'       => __( 'Parent Style', 'igv' ),
+		'parent_item_colon' => __( 'Parent Style:', 'igv' ),
+		'edit_item'         => __( 'Edit Style', 'igv' ),
+		'update_item'       => __( 'Update Style', 'igv' ),
+		'add_new_item'      => __( 'Add New Style', 'igv' ),
+		'new_item_name'     => __( 'New Style Name', 'igv' ),
+		'menu_name'         => __( 'Styles', 'igv' ),
 	);
 
 	$args = array(
-		'hierarchical'          => false,
-		'labels'                => $labels,
-		'show_ui'               => true,
-		'show_admin_column'     => true,
-		'update_count_callback' => '_update_post_term_count',
-		'query_var'             => true,
-		'rewrite'               => array( 'slug' => 'theme' ),
+		'hierarchical'      => false,
+		'labels'            => $labels,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'query_var'         => true,
+		'rewrite'           => array( 'slug' => 'style' ),
+    'show_in_rest'      => true,
 	);
 
-	register_taxonomy( 'theme', 'book', $args );
+	register_taxonomy( 'style', array( 'album' ), $args );
+
+  // Add new taxonomy, make it hierarchical (like categories)
+	$labels = array(
+		'name'              => _x( 'Years', 'taxonomy general name', 'igv' ),
+		'singular_name'     => _x( 'Year', 'taxonomy singular name', 'igv' ),
+		'search_items'      => __( 'Search Years', 'igv' ),
+		'all_items'         => __( 'All Years', 'igv' ),
+		'parent_item'       => __( 'Parent Year', 'igv' ),
+		'parent_item_colon' => __( 'Parent Year:', 'igv' ),
+		'edit_item'         => __( 'Edit Year', 'igv' ),
+		'update_item'       => __( 'Update Year', 'igv' ),
+		'add_new_item'      => __( 'Add New Year', 'igv' ),
+		'new_item_name'     => __( 'New Year Name', 'igv' ),
+		'menu_name'         => __( 'Years', 'igv' ),
+	);
+
+	$args = array(
+		'hierarchical'      => false,
+		'labels'            => $labels,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'query_var'         => true,
+		'rewrite'           => array( 'slug' => 'year' ),
+    'show_in_rest'      => true,
+	);
+
+	register_taxonomy( 'year', array( 'album' ), $args );
 }
 ?>
