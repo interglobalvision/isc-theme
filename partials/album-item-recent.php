4294a38b9787a3383cf81bed72aaddfcb1a4916c
mikeyrayvon <mikey@interglobal.vision>
2020-07-08 20:30:54 +0200

    add recent album item

--- partials/album-item-recent.php
+++ partials/album-item-recent.php
@@ -5,13 +5,15 @@ $styles = get_the_terms($post->ID, 'style');
 ?>
 <article <?php post_class('grid-item item-s-12 item-m-4 grid-row'); ?> id="album-<?php the_ID(); ?>">
   <div class="grid-item no-gutter item-s-6">
-    <?php the_post_thumbnail('full'); ?>
+    <a href="<?php the_permalink(); ?>">
+      <?php the_post_thumbnail('full'); ?>
+    </a>
   </div>
   <div class="grid-item no-gutter item-s-6 grid-column justify-between">
     <div>
       <h3 class="u-visuallyhidden"><?php the_title(); ?></h3>
-      <div><span>Artist</span></div>
-      <div><span>Title</span></div>
+      <div><a href="<?php the_permalink(); ?>">Artist</a></div>
+      <div><a href="<?php the_permalink(); ?>">Title</a></div>
     </div>
   <?php if ($styles) { ?>
     <div>
