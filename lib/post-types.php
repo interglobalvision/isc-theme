463faa09f27263e71face1bc88f113bac7f2a12f
mikeyrayvon <mikey@interglobal.vision>
2020-07-16 17:30:26 +0200

    disable tracks archive

--- lib/post-types.php
+++ lib/post-types.php
@@ -106,7 +106,7 @@ function register_cpt_track() {
     'show_in_nav_menus' => true,
     'publicly_queryable' => true,
     'exclude_from_search' => false,
-    'has_archive' => 'collection',
+    'has_archive' => false,
     'query_var' => true,
     'can_export' => true,
     'rewrite' => true,
