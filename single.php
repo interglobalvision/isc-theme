35fab81b434651a262b359ad31438adaa6745945
mikeyrayvon <mikey@interglobal.vision>
2020-07-18 18:43:44 +0200

    scaffold single post header

--- single.php
+++ single.php
@@ -3,8 +3,7 @@ get_header();
 ?>
 
 <main id="main-content">
-  <section class="border-box">
-
+  <section>
     <div class="container">
       <div class="grid-row">
 
@@ -13,6 +12,22 @@ get_header();
         while (have_posts()) {
           the_post();
       ?>
+        <header id="post-header" class="item-s-12 grid-row">
+          <div id="post-title-holder" class="grid-item item-s-12 item-l-6">
+            <h1>Title</h1>
+          </div>
+          <div id="post-details-holder" class="item-s-12 item-l-6 grid-row">
+            <div class="grid-item item-s-8 item-l-6 offset-l-2">
+              Author
+            </div>
+            <div class="grid-item item-s-4">
+              Date
+            </div>
+          </div>
+        </header>
+        <div class="grid-item item-s-12">
+          <?php the_post_thumbnail('full', array('id' => 'post-featured-image')); ?>
+        </div>
         <div class="grid-item item-s-12">
           <?php the_content(); ?>
         </div>
@@ -23,7 +38,6 @@ get_header();
 
       </div>
     </div>
-
   </section>
 </main>
 
