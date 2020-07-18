9dbe50514b562a6d7f6dddc3004c79934f5b5b92
mikeyrayvon <mikey@interglobal.vision>
2020-07-18 18:26:05 +0200

    fix post item layout and date

--- partials/post-item.php
+++ partials/post-item.php
@@ -1,25 +1,29 @@
-<article <?php post_class('grid-item no-gutter item-s-12 item-m-6 grid-row margin-bottom-basic'); ?> id="post-<?php the_ID(); ?>">
-  <div class="grid-item no-gutter item-s-12 grid-row">
+<article <?php post_class('grid-item no-gutter item-s-12 item-m-6 grid-column margin-bottom-basic'); ?> id="post-<?php the_ID(); ?>">
+  <div class="grid-row">
     <div class="grid-item item-s-auto flex-grow offset-l-3">
       <span>Author</span>
     </div>
     <div class="grid-item">
-      <?php the_date(); ?>
+      <?php echo get_the_date(); ?>
     </div>
   </div>
-  <div class="grid-item item-s-12">
+  <div class="grid-item">
     <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full'); ?></a>
   </div>
-  <div class="grid-item item-s-3">
-    <span>FEATURE</span>
+  <div class="grid-row">
+    <div class="grid-item item-s-3">
+      <span>FEATURE</span>
+    </div>
+    <div class="grid-item item-s-9">
+      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    </div>
   </div>
-  <div class="grid-item item-s-9">
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-  </div>
-  <div class="grid-item item-s-12">
+  <div class="grid-item">
     <?php the_excerpt(); ?>
   </div>
-  <div class="grid-item offset-s-9">
-    <a href="<?php the_permalink(); ?>">-></a>
+  <div class="grid-item flex-grow grid-row align-content-end">
+    <div class="offset-s-9">
+      <a href="<?php the_permalink(); ?>">-></a>
+    </div>
   </div>
 </article>
