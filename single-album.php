0a058d4d2fb2da14431421fef0ef33269fb44e3e
mikeyrayvon <mikey@interglobal.vision>
2020-07-23 10:45:35 +0200

    update thumb sizes

--- single-album.php
+++ single-album.php
@@ -27,7 +27,7 @@ if (have_posts()) {
     <div class="container">
       <div class="grid-row">
         <div class="grid-item item-s-12 item-m-5 margin-bottom-basic">
-          <?php the_post_thumbnail('full', array( 'alt' => get_the_title() . ' album cover', 'data-no-lazysizes' => 'true')); ?>
+          <?php the_post_thumbnail('gallery', array( 'alt' => get_the_title() . ' album cover', 'data-no-lazysizes' => 'true')); ?>
         </div>
         <div class="grid-item item-s-12 item-m-6 offset-m-1 grid-row no-gutter align-content-start margin-bottom-basic">
           <header class="grid-item item-s-12 margin-bottom-small">
