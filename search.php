6483f2dfc28aa38b824b9b6a7a1e6f4ef54acf23
mikeyrayvon <mikey@interglobal.vision>
2020-07-23 09:46:24 +0200

    add footer and wrapping divs to search template

--- search.php
+++ search.php
@@ -5,18 +5,25 @@ global $wp_query;
 $max_pages = $wp_query->max_num_pages;
 $current_page = (get_query_var('paged')) ? get_query_var('paged') : 1;
 ?>
-<?php get_search_form(); ?>
-<section>
-  <div class="container">
-    <div id="search-results" data-max-pages="<?php echo $max_pages; ?>" class="grid-row">
-      <?php
-      if (have_posts()) {
-        while (have_posts()) {
-          the_post();
-          get_template_part('partials/search-item');
+
+<main id="main-content">
+  <section class="padding-top-basic padding-bottom-large">
+    <div class="container">
+      <?php get_search_form(); ?>
+      <div id="search-results" data-max-pages="<?php echo $max_pages; ?>" class="grid-row">
+        <?php
+        if (have_posts()) {
+          while (have_posts()) {
+            the_post();
+            get_template_part('partials/search-item');
+          }
         }
-      }
-      ?>
+        ?>
+      </div>
     </div>
-  </div>
-</section>
+  </section>
+</main>
+
+<?php
+get_footer();
+?>
