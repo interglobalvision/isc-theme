11fac9d15c015333c4e0bb0da9c1bfeb146f9ab2
mikeyrayvon <mikey@interglobal.vision>
2020-07-23 09:45:21 +0200

    set album archive item thumb size

--- archive-album.php
+++ archive-album.php
@@ -39,7 +39,7 @@ if (have_posts()) {
         <article <?php post_class('grid-item item-s-6 item-m-4 item-l-3'); ?> id="album-<?php the_ID(); ?>" data-filter="<?php echo $filter; ?>">
           <a href="<?php the_permalink() ?>">
             <h3 class="u-visuallyhidden"><?php the_title(); ?></h3>
-            <div><?php the_post_thumbnail('full'); ?></div>
+            <div><?php the_post_thumbnail('album-item'); ?></div>
           </a>
         </article>
 
