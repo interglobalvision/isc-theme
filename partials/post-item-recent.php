42faf015532f9e6924529bb79798b9d4fbc81251
mikeyrayvon <mikey@interglobal.vision>
2020-07-26 20:54:28 +0200

    add recent post thumb size

--- partials/post-item-recent.php
+++ partials/post-item-recent.php
@@ -1,6 +1,6 @@
 <article <?php post_class('grid-item no-gutter item-s-12 item-m-4 grid-row'); ?> id="post-<?php the_ID(); ?>">
   <div class="grid-item item-s-12 margin-bottom-small">
-    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full'); ?></a>
+    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('recent-post'); ?></a>
   </div>
   <div class="grid-item item-s-12">
     <h2 class="font-size-large font-cond margin-bottom-tiny"><?php the_title(); ?></h2>
