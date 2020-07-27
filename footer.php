270068370e4a56fb0845fc74809b61061d8bb9d8
mikeyrayvon <mikey@interglobal.vision>
2020-07-27 22:53:02 +0200

    add social icons

--- footer.php
+++ footer.php
@@ -34,8 +34,25 @@ $options = get_site_option('_igv_site_options');
           </div>
         <?php } ?>
           <div id="social-holder" class="grid-item no-gutter grid-row">
-            <div class="grid-item">Social Media</div>
-            <div class="grid-item">Social Media</div>
+          <?php if (!empty($options['socialmedia_instagram'])) { ?>
+            <div class="grid-item">
+              <a href="https://instagram.com/<?php echo $options['socialmedia_instagram']; ?>" class="social-icon">
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/social-instagram.png" />
+              </a>
+            </div>
+          <?php } if (!empty($options['socialmedia_facebook_url'])) { ?>
+            <div class="grid-item">
+              <a href="<?php echo $options['socialmedia_facebook_url']; ?>" class="social-icon">
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/social-facebook.png" />
+              </a>
+            </div>
+          <?php } if (!empty($options['socialmedia_twitter'])) { ?>
+            <div class="grid-item">
+              <a href="https://twitter.com/<?php echo $options['socialmedia_twitter']; ?>" class="social-icon">
+                <img src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/social-twitter.png" />
+              </a>
+            </div>
+          <?php } ?>
             <div>&nbsp;</div>
           </div>
         </div>
