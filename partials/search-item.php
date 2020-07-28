d161de764edcfa57875e22ee9fad467a376ea5dc
mikeyrayvon <mikey@interglobal.vision>
2020-07-28 12:07:30 +0200

    add search styling

--- partials/search-item.php
+++ partials/search-item.php
@@ -1,24 +1,28 @@
-<article <?php post_class('grid-item item-s-12 item-m-6 grid-row flex-nowrap search-result'); ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class('grid-item item-s-12 item-m-6 grid-row flex-nowrap search-result margin-bottom-small'); ?> id="post-<?php the_ID(); ?>">
   <div>
     <a href="<?php the_permalink(); ?>">
-      <div class="search-thumb-holder" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>')"></div>
+      <div class="search-thumb-holder margin-right-tiny" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>')"></div>
     </a>
   </div>
   <div class="flex-grow">
-    <div>
-      <span><?php
-        switch (get_post_type()) {
-          case 'post':
-            echo 'Feature';
-            break;
-          case 'album':
-            echo 'Album';
-            break;
-          default:
-            echo ' ';
-        }
-      ?></span>
-    </div>
-    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+    <a href="<?php the_permalink(); ?>">
+      <div>
+        <span class="font-uppercase"><?php
+          switch (get_post_type()) {
+            case 'post':
+              echo 'Feature';
+              break;
+            case 'album':
+              echo 'Album';
+              break;
+            default:
+              echo ' ';
+          }
+        ?></span>
+      </div>
+      <div>
+        <?php the_title(); ?>
+      </div>
+    </a>
   </div>
 </article>
