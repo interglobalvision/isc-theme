bc64fe5d47327b143fbef03ce3ab2a882c925b43
mikeyrayvon <mikey@interglobal.vision>
2020-07-28 09:47:13 +0200

    add wolf logo to footer

--- footer.php
+++ footer.php
@@ -7,7 +7,8 @@ $options = get_site_option('_igv_site_options');
         <div class="grid-item item-s-12 item-l-4 no-gutter grid-column justify-between">
           <div class="grid-item">
             <div class="not-mobile">
-              <span class="font-bold">ISCHiFi</span>
+              <div class="margin-bottom-tiny"><span class="font-bold">ISCHiFi</span></div>
+              <div><?php get_template_part('assets/logo.svg'); ?></div>
             </div>
           </div>
         <?php if (!empty($options['mailchimp_action'])) { ?>
