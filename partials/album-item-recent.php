e49ca575aa88b6fc5899481a2133b35596f177eb
mikeyrayvon <mikey@interglobal.vision>
2020-07-29 20:50:39 +0200

    adjust recent album thumb size

--- partials/album-item-recent.php
+++ partials/album-item-recent.php
@@ -4,12 +4,12 @@ $title = get_post_meta($post->ID, '_igv_album_title', true);
 $styles = get_the_terms($post->ID, 'style');
 ?>
 <article <?php post_class('album-item-recent grid-item item-s-12 item-m-4 grid-row margin-bottom-small'); ?> id="post-<?php the_ID(); ?>">
-  <div class="grid-item no-gutter item-m-12 item-l-7 margin-right-tiny">
+  <div class="grid-item no-gutter item-m-12 item-l-6 margin-right-tiny">
     <a href="<?php the_permalink(); ?>">
       <?php the_post_thumbnail('album-item', ['class' => 'recent-album-thumb']); ?>
     </a>
   </div>
-  <div class="grid-item no-gutter grid-column justify-between item-m-12 item-l-auto">
+  <div class="grid-item no-gutter grid-column justify-between item-m-12 item-l-6">
     <div>
       <h3 class="u-visuallyhidden"><?php the_title(); ?></h3>
       <div>
