ebf6e9403509c33c3408be7f8184e09e01151afb
mikeyrayvon <mikey@interglobal.vision>
2020-08-16 22:13:43 +0200

    update player play button. add player mobile icons

---
+++ assets/player-pause-mobile.svg.php
@@ -0,0 +1,10 @@
+<svg class="icon-player-pause-mobile" width="13px" height="16px" viewBox="0 0 13 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+    <g transform="translate(-201.000000, -53.000000)" fill="#FFFFFF">
+      <g transform="translate(201.000000, 53.000000)">
+        <rect x="0" y="0" width="5" height="16"></rect>
+        <rect x="8" y="0" width="5" height="16"></rect>
+      </g>
+    </g>
+  </g>
+</svg>
---
+++ assets/player-play-mobile.svg.php
@@ -0,0 +1,9 @@
+<svg class="icon-player-play-mobile" width="14px" height="15px" viewBox="0 0 14 15" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+    <g transform="translate(-200.000000, -53.000000)" fill="#FFFFFF" stroke="#FFFFFF">
+      <g transform="translate(201.000000, 54.000000)">
+        <polygon points="0.333333333 0.333333333 12.6666667 6.5 0.333333333 12.6666667 0.333333333 6.5"></polygon>
+      </g>
+    </g>
+  </g>
+</svg>
