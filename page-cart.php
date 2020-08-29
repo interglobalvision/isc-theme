d3ecdbafe4e02268fca266ea4ef32f7d850ebb91
mikeyrayvon <mikey@interglobal.vision>
2020-08-29 21:05:23 +0200

    scaffold cart page

--- page-cart.php
+++ page-cart.php
@@ -9,33 +9,36 @@ if (have_posts()) {
     the_post();
 ?>
   <section class="gws-cart padding-bottom-large padding-top-mid mobile-margin-top">
-    <div id="cart-items" class="gws-cart-items">
-      <div class="gws-cart-item">
-        <div class="grid-row">
-          <div class="grid-row item-s-12 item-l-6 offset-l-1">
-            <div class="grid-item item-s-6 margin-bottom-small">
-              <div class="gws-cart-thumb"></div>
+    <div class="container">
+      <div id="cart-items" class="gws-cart-items">
+        <div class="gws-cart-item margin-bottom-basic">
+          <div class="grid-row">
+            <div class="grid-row item-s-4 item-m-2">
+              <div class="grid-item gws-cart-thumb"></div>
             </div>
-
-            <div class="grid-item item-s-6 margin-bottom-small">
-              <div class="gws-cart-title"></div>
+            <div class="grid-row item-s-8 item-m-10">
+              <div class="grid-row item-s-12 item-m-6 margin-bottom-small align-content-start">
+                <div class="grid-item item-s-12 margin-bottom-small">
+                  <span class="gws-cart-title"></span>
+                </div>
+                <div class="grid-row item-s-12 align-content-start">
+                  <div class="grid-item item-s-6 item-m-3">
+                    <span class="font-cond">Qty</span>
+                  </div>
+                  <div class="grid-item item-s-6 item-m-3">
+                    <input type="number" min="1" max="100" class="gws-cart-quantity" />
+                  </div>
+                </div>
+              </div>
+              <div class="item-s-12 item-m-6 grid-row align-content-start">
+                <div class="grid-item item-s-6 item-m-12 margin-bottom-small">
+                  <span class="font-cond">Price</span>
+                </div>
+                <div class="grid-item item-s-6 item-m-12">
+                  <span>$<span class="gws-cart-item-subtotal"></span></span>
+                </div>
+              </div>
             </div>
-          </div>
-
-          <div class="grid-row item-s-12 item-l-4">
-            <div class="grid-item item-s-6 margin-bottom-small">
-              <span>$<span class="gws-cart-item-subtotal"></span></span>
-            </div>
-            <div class="grid-item item-s-4 margin-bottom-small">
-              <input type="number" min="1" max="100" class="gws-cart-quantity" />
-            </div>
-            <div class="grid-item item-s-2 margin-bottom-small">
-              <a class="gws-cart-remove u-pointer">&times;</a>
-            </div>
-          </div>
-
-          <div class="grid-item item-s-12 item-l-10 offset-l-1 margin-bottom-small">
-            <div class="border"></div>
           </div>
         </div>
       </div>
