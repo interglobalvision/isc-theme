e8ac2d27606d0dad7bffbefc262d0b5e8ce46305
mikeyrayvon <mikey@interglobal.vision>
2020-08-29 10:17:15 +0200

    add post category to recent post item

--- partials/post-item-recent.php
+++ partials/post-item-recent.php
@@ -3,9 +3,13 @@
     <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('recent-post'); ?></a>
   </div>
   <div class="grid-item item-s-12">
+    <span class="font-uppercase font-size-small"><?php
+      $category = get_the_category($post->ID);
+      echo $category[0]->slug === 'uncategorized' ? 'Feature' : $category[0]->name;
+    ?></span>
     <h2 class="font-size-large font-cond margin-bottom-tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
   </div>
-  <div class="grid-item no-gutter item-s-12">
+  <div class="item-s-12 grid-row">
     <div class="grid-item item-s-12">
       <?php guest_authors($post->ID); ?>
     </div>
