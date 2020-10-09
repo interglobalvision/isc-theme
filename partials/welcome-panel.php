373145ea16916e4a9fa420a4d7fb69c8ac5bbc4b
mikeyrayvon <mikey@interglobal.vision>
2020-10-09 21:20:52 +0200

    change welcome close button

--- partials/welcome-panel.php
+++ partials/welcome-panel.php
@@ -4,12 +4,14 @@ $options = get_site_option('_igv_site_options');
 
 <div id="welcome-panel" class="grid-row align-items-center align-content-center padding-top-mid padding-bottom-mid">
   <?php if (!empty($options['welcome_text'])) { ?>
-  <div id="welcome-content" class="grid-item item-s-8 font-mono">
+  <div id="welcome-content" class="grid-item item-s-8 offset-s-2 font-mono">
     <?php echo apply_filters('the_content', $options['welcome_text']); ?>
   </div>
   <?php } ?>
-  <div class="grid-item item-s-8">
+  <div class="grid-item item-s-8 offset-s-2 margin-bottom-small">
     <p>Our website uses cookies<br><a href="<?php echo home_url('privacy-policy'); ?>" class="font-size-small">Read our privacy policy.</a></p>
   </div>
-  <div id="close-welcome"><img class="close-welcome" src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/close.png" /></div>
+  <div class="grid-item item-s-8 offset-s-2 text-align-center font-size-small font-uppercase">
+    <div class="small-button close-welcome"><span>I Agree</span></div>
+  </div>
 </div>
