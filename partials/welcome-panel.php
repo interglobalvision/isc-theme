a0d4e3a3f92fc189d44c4ad73120e77607ebd5bb
mikeyrayvon <mikey@interglobal.vision>
2020-11-18 09:23:11 +0100

    adjust welcome panel item sizing

--- partials/welcome-panel.php
+++ partials/welcome-panel.php
@@ -4,14 +4,14 @@ $options = get_site_option('_igv_site_options');
 
 <div id="welcome-panel" class="grid-row align-items-center align-content-center padding-top-mid padding-bottom-mid">
   <?php if (!empty($options['welcome_text'])) { ?>
-  <div id="welcome-content" class="grid-item item-s-8 offset-s-2 font-mono">
+  <div id="welcome-content" class="grid-item item-s-12 item-m-8 offset-m-2 font-mono">
     <?php echo apply_filters('the_content', $options['welcome_text']); ?>
   </div>
   <?php } ?>
-  <div class="grid-item item-s-8 offset-s-2 margin-bottom-small">
+  <div class="grid-item item-s-12 item-m-8 offset-m-2 margin-bottom-small">
     <p>Our website uses cookies<br><a href="<?php echo home_url('privacy-policy'); ?>" class="font-size-small">Read our privacy policy.</a></p>
   </div>
-  <div class="grid-item item-s-8 offset-s-2 text-align-center font-size-small font-uppercase">
+  <div class="grid-item item-s-12 item-m-8 offset-m-2 text-align-center font-size-small font-uppercase">
     <div class="small-button close-welcome"><span>I Agree</span></div>
   </div>
 </div>
