156c6c2e06efdab86847027f507818cb7e8dd4b2
mikeyrayvon <mikey@interglobal.vision>
2020-11-20 19:22:20 +0100

    add new streaming services and layoutg

--- partials/album-streaming.php
+++ partials/album-streaming.php
@@ -7,57 +7,85 @@ $related_track = get_posts(array(
   'meta_value' => $post->ID,
 ));
 ?>
-<div id="album-streaming" class="grid-item item-s-12 no-gutter grid-row">
-  <div class="grid-item margin-bottom-tiny">
-    <span class="font-cond">Stream:</span>
+<div id="album-streaming" class="grid-item item-s-12 no-gutter">
+  <div class="grid-row">
+    <div class="grid-item item-s-12 item-m-3 margin-bottom-tiny">
+      <span class="font-cond">Hi-Fi Stream:</span>
+    </div>
+    <div class="flex-grow">
+    <?php
+    if (!empty($related_track)) {
+      $track_id = $related_track[0]->ID;
+      $title = get_the_title($track_id);
+      $thumb_url = get_the_post_thumbnail_url($track_id);
+      $soundcloud_url = get_post_meta($track_id, '_igv_track_soundcloud', true);
+      if (!empty($soundcloud_url)) {
+    ?>
+      <div class="u-inline-block margin-bottom-tiny">
+        <div class="stream-button player-skip album-stream u-pointer"
+          data-title="<?php echo $title; ?>"
+          data-thumb="<?php echo !empty($thumb_url) ? $thumb_url : get_the_post_thumbnail_url($post->ID); ?>"
+          data-soundcloud="<?php echo $soundcloud_url; ?>"
+          data-url="<?php echo get_permalink(); ?>"
+          data-id="<?php echo $track_id; ?>">
+            <?php get_template_part('assets/streaming-iscroom.svg'); ?>
+          </div>
+      </div>
+    <?php
+      }
+    }
+
+    $services = array(
+      'qobuz',
+      'tidal'
+    );
+
+    foreach ($services as $service) {
+      $meta_key = '_igv_album_' . $service . '_url';
+      $url = get_post_meta($post->ID, $meta_key, true);
+      $button_asset = 'assets/streaming-' . $service . '.svg';
+
+      if (!empty($url)) {
+    ?>
+      <div class="u-inline-block margin-bottom-tiny">
+        <a class="stream-button u-pointer streaming-service" href="<?php echo $url; ?>" target="_blank">
+          <?php get_template_part($button_asset); ?>
+        </a>
+      </div>
+    <?php
+      }
+    }
+    ?>
+    </div>
   </div>
-  <div class="flex-grow">
-  <?php
-  if (!empty($related_track)) {
-    $track_id = $related_track[0]->ID;
-    $title = get_the_title($track_id);
-    $thumb_url = get_the_post_thumbnail_url($track_id);
-    $soundcloud_url = get_post_meta($track_id, '_igv_track_soundcloud', true);
-    if (!empty($soundcloud_url)) {
-  ?>
-    <div class="u-inline-block margin-bottom-tiny">
-      <div class="stream-button player-skip album-stream u-pointer"
-        data-title="<?php echo $title; ?>"
-        data-thumb="<?php echo !empty($thumb_url) ? $thumb_url : get_the_post_thumbnail_url($post->ID); ?>"
-        data-soundcloud="<?php echo $soundcloud_url; ?>"
-        data-url="<?php echo get_permalink(); ?>"
-        data-id="<?php echo $track_id; ?>">
-          <?php get_template_part('assets/streaming-iscroom.svg'); ?>
-        </div>
+  <div class="grid-row">
+    <div class="grid-item item-s-12 item-m-3 margin-bottom-tiny">
+      <span class="font-cond">Standard Stream:</span>
     </div>
-  <?php
+    <div class="flex-grow">
+    <?php
+    $services = array(
+      'amazon',
+      'apple',
+      'spotify',
+    );
+
+    foreach ($services as $service) {
+      $meta_key = '_igv_album_' . $service . '_url';
+      $url = get_post_meta($post->ID, $meta_key, true);
+      $button_asset = 'assets/streaming-' . $service . '.svg';
+
+      if (!empty($url)) {
+    ?>
+      <div class="u-inline-block margin-bottom-tiny">
+        <a class="stream-button u-pointer streaming-service" href="<?php echo $url; ?>" target="_blank">
+          <?php get_template_part($button_asset); ?>
+        </a>
+      </div>
+    <?php
+      }
     }
-  }
-
-  $services = array(
-    'tidal',
-    'apple',
-    'spotify',
-    'youtube',
-    'bandcamp',
-    'soundcloud'
-  );
-
-  foreach ($services as $service) {
-    $meta_key = '_igv_album_' . $service . '_url';
-    $url = get_post_meta($post->ID, $meta_key, true);
-    $button_asset = 'assets/streaming-' . $service . '.svg';
-
-    if (!empty($url)) {
-  ?>
-    <div class="u-inline-block margin-bottom-tiny">
-      <a class="stream-button u-pointer streaming-service" href="<?php echo $url; ?>" target="_blank">
-        <?php get_template_part($button_asset); ?>
-      </a>
+    ?>
     </div>
-  <?php
-    }
-  }
-  ?>
   </div>
 </div>
