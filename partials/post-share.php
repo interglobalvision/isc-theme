983b6b3f3e0442d89d1a50f2c0bad0e416680cd1
mikeyrayvon <mikey@interglobal.vision>
2020-11-20 21:43:49 +0100

    add share icons

--- partials/post-share.php
+++ partials/post-share.php
@@ -4,13 +4,13 @@
   </div>
   <ul class="grid-row align-items-center">
     <li class="grid-item">
-      <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode( get_permalink() ); ?>">F</a>
+      <a class="social-icon" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode( get_permalink() ); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/social-facebook.png" /></a>
     </li>
     <li class="grid-item">
-      <a href="https://twitter.com/intent/tweet?text=Check%20this%20out%3A%20<?php echo urlencode( get_permalink() ); ?>">T</a>
+      <a class="social-icon" href="https://twitter.com/intent/tweet?text=Check%20this%20out%3A%20<?php echo urlencode( get_permalink() ); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/social-twitter.png" /></a>
     </li>
     <li class="grid-item">
-      <a href="mailto:?&subject=<?php echo rawurlencode( get_the_title() ); ?>&body=Check%20this%20out:%0D<?php echo urlencode( get_permalink() ); ?>">M</a>
+      <a class="social-icon" href="mailto:?&subject=<?php echo rawurlencode( get_the_title() ); ?>&body=Check%20this%20out:%0D<?php echo urlencode( get_permalink() ); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/social-email.png" /></a>
     </li>
   </ul>
 </div>
