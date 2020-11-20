6bcdb699a6ddd7c229a038e551ef0507acd89a9b
mikeyrayvon <mikey@interglobal.vision>
2020-11-20 23:06:31 +0100

    update share copy

--- partials/post-share.php
+++ partials/post-share.php
@@ -1,16 +1,25 @@
+<?php
+$permalink = get_permalink();
+$title = get_the_title();
+$facebook_post = urlencode( $permalink );
+$tweet = rawurlencode('From In Sheep\'s Clothing HiFi: ' . $title . ' ' . $permalink);
+$email_subject = rawurlencode('From In Sheep\'s Clothing HiFi');
+$email_body = rawurlencode('From In Sheep\'s Clothing HiFi: ' . $title . ' ' . $permalink);
+?>
+
 <div class="grid-row align-items-center">
   <div class="grid-item item-s-2">
     <span>Share:</span>
   </div>
   <ul class="grid-row align-items-center">
     <li class="grid-item">
-      <a class="social-icon" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode( get_permalink() ); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/social-facebook.png" /></a>
+      <a class="social-icon" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $facebook_post; ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/social-facebook.png" /></a>
     </li>
     <li class="grid-item">
-      <a class="social-icon" href="https://twitter.com/intent/tweet?text=Check%20this%20out%3A%20<?php echo urlencode( get_permalink() ); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/social-twitter.png" /></a>
+      <a class="social-icon" href="https://twitter.com/intent/tweet?text=<?php echo $tweet; ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/social-twitter.png" /></a>
     </li>
     <li class="grid-item">
-      <a class="social-icon" href="mailto:?&subject=<?php echo rawurlencode( get_the_title() ); ?>&body=Check%20this%20out:%0D<?php echo urlencode( get_permalink() ); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/social-email.png" /></a>
+      <a class="social-icon" href="mailto:?&subject=<?php echo $email_subject; ?>&body=<?php echo $email_body; ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/dist/img/social-email.png" /></a>
     </li>
   </ul>
 </div>
