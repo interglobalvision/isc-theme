fd9a7229923612dddd53b16cadbf3e400af59b5c
mikeyrayvon <mikey@interglobal.vision>
2020-11-21 08:42:46 +0100

    adjust slider padding

--- partials/home-featured-albums.php
+++ partials/home-featured-albums.php
@@ -2,7 +2,7 @@
 $featured_albums = get_post_meta($post->ID, '_igv_featured_albums', true);
 ?>
 
-<section class="background-woodgrain padding-top-mid padding-bottom-basic">
+<section class="background-woodgrain padding-top-small">
 
   <?php get_template_part('assets/woodgrain.svg'); ?>
 
@@ -15,7 +15,7 @@ if (!empty($featured_albums)) {
     <?php
       foreach ($featured_albums as $album_id) {
     ?>
-      <article <?php post_class('swiper-slide album-featured-slide text-align-center'); ?> id="post-<?php echo $album_id; ?>">
+      <article <?php post_class('swiper-slide album-featured-slide text-align-center padding-top-basic padding-bottom-basic'); ?> id="post-<?php echo $album_id; ?>">
         <a href="<?php echo get_the_permalink($album_id); ?>">
           <?php echo get_the_post_thumbnail($album_id, 'full', array('data-no-lazysizes' => 'true')); ?>
           <div class="slide-info-icon not-desktop"><?php get_template_part('assets/info.svg'); ?></div>
