eea9d2d55b5a64ac38e410759f8095abc48273e0
mikeyrayvon <mikey@interglobal.vision>
2020-11-29 21:31:42 +0100

    adjust category layout

--- single.php
+++ single.php
@@ -15,17 +15,22 @@ if (have_posts()) {
       <div class="grid-row">
         <header id="post-header" class="item-s-12 grid-row margin-bottom-tiny">
           <div id="post-title-holder" class="grid-item item-s-12 item-l-6 margin-bottom-small">
-            <div class="margin-bottom-tiny">
-              <span class="font-uppercase"><?php
-                $category = get_the_category($post->ID);
-                echo $category[0]->slug === 'uncategorized' ? 'Feature' : $category[0]->name;
-              ?></span>
-            </div>
+            <?php
+              $category = get_the_category($post->ID);
+              if ($category[0]->slug !== 'uncategorized') {
+            ?>
+              <div class="margin-bottom-tiny">
+                <a href="<?php echo get_category_link($category[0]); ?>" class="font-uppercase font-size-small"><?php echo $category[0]->name; ?></a>
+              </div>
+            <?php
+              }
+            ?>
+
             <h1 class="font-size-extra font-cond"><?php the_title(); ?></h1>
           </div>
-          <div id="post-details-holder" class="item-s-12 item-l-6 margin-bottom-small margin-top-tiny font-size-small">
+          <div id="post-details-holder" class="item-s-12 item-l-6 margin-bottom-small font-size-small">
             <div class="grid-row">
-              <div class="grid-item item-s-8 item-l-6 offset-l-2">
+              <div class="grid-item item-s-8 item-l-6 offset-l-2 margin-bottom-tiny">
                 <div><span>Written By:&nbsp;</span></div>
                 <?php guest_authors($post->ID); ?>
               </div>
