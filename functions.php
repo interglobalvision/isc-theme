a7246e658bb53fe71ebcb02941e06e1428c53deb
mikeyrayvon <mikey@interglobal.vision>
2021-05-23 19:25:10 +0200

    increase css version

--- functions.php
+++ functions.php
@@ -73,7 +73,7 @@ function scripts_and_styles_method() {
   wp_enqueue_script('javascript-main');
 
   // Enqueue style
-  wp_enqueue_style( 'style-site', get_stylesheet_directory_uri() . '/dist/css/site.css', [], '2.0.10' );
+  wp_enqueue_style( 'style-site', get_stylesheet_directory_uri() . '/dist/css/site.css', [], '2.0.11' );
 
   // dashicons for admin
   if (is_admin()) {
