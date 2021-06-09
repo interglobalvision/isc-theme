9b731e0901fdba43465e793d038cf34bb230e92a
mikeyrayvon <mikey@interglobal.vision>
2021-06-09 18:52:01 +0200

    add event post type

--- lib/post-types.php
+++ lib/post-types.php
@@ -14,6 +14,12 @@ function add_menu_icons_styles(){
 #menu-posts-product .dashicons-admin-post:before {
     content: '\f174';
 }
+#menu-posts-partner .dashicons-admin-post:before {
+    content: '\f307';
+}
+#menu-posts-event .dashicons-admin-post:before {
+    content: '\f508';
+}
 </style>
 
 <?php
@@ -186,4 +192,60 @@ function register_cpt_partner() {
   );
 
   register_post_type( 'partner', $args );
+}
+
+add_action( 'init', 'register_cpt_event' );
+
+function register_cpt_event() {
+
+  $labels = array(
+    'name' => _x( 'Events', 'igv' ),
+    'singular_name' => _x( 'Event', 'igv' ),
+    'add_new' => _x( 'Add New', 'igv' ),
+    'add_new_item' => _x( 'Add New Event', 'igv' ),
+    'edit_item' => _x( 'Edit Event', 'igv' ),
+    'new_item' => _x( 'New Event', 'igv' ),
+    'view_item' => _x( 'View Event', 'igv' ),
+    'search_items' => _x( 'Search Events', 'igv' ),
+    'not_found' => _x( 'No events found', 'igv' ),
+    'not_found_in_trash' => _x( 'No events found in Trash', 'igv' ),
+    'parent_item_colon' => _x( 'Parent Event:', 'igv' ),
+    'menu_name' => _x( 'Events', 'igv' ),
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'hierarchical' => false,
+
+    'supports' => array( 'title', 'editor', 'thumbnail' ),
+
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+
+    'show_in_nav_menus' => true,
+    'publicly_queryable' => true,
+    'exclude_from_search' => false,
+    'has_archive' => 'collection',
+    'query_var' => true,
+    'can_export' => true,
+    'rewrite' => true,
+    'capability_type' => 'post',
+
+    'taxonomies' => array('post_tag'),
+
+    'show_in_rest'        => true,
+    /*'template'          => array(
+      array( 'core/heading', array(
+        'level' => '5', 'content' => 'Some List' ) ),
+      array( 'core/list' ),
+      array( 'core/heading', array(
+        'level' => '5', 'content' => 'Some Text' ) ),
+      array( 'core/paragraph' )
+    ),*/
+		//'template_lock'     => 'all',
+  );
+
+  register_post_type( 'event', $args );
 }
