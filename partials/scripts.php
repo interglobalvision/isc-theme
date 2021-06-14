0877cb5a5bd163e8c4a5a252ef7d6d077324f8af
mikeyrayvon <mikey@interglobal.vision>
2021-06-14 19:39:32 +0200

    include eventbrite script in footer

--- partials/scripts.php
+++ partials/scripts.php
@@ -1,6 +1,7 @@
 <section id="scripts">
   <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
   <script>window.jQuery || document.write('<script src="<?php bloginfo('stylesheet_directory'); ?>/dist/static/vendor/jquery-3.1.1.min.js"><\/script>')</script>
+  <script src="https://www.eventbrite.com/static/widgets/eb_widgets.js"></script>
   <?php wp_footer(); ?>
 <?php
 
