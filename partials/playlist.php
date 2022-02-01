7d3511e6a973cad486150996354d90379056797d
mikeyrayvon <mikey@interglobal.vision>
2022-02-01 12:16:34 +0100

    use cloudinary url for playlist. have playlist image element when no image available

--- partials/playlist.php
+++ partials/playlist.php
@@ -9,17 +9,19 @@ if (!empty($playlist)) {
   <ul id="playlist-items" class="container padding-top-tiny padding-bottom-large grid-row">
   <?php
     foreach($playlist as $track_index => $track_id) {
-      $related_album = get_post_meta($track_id, '_igv_track_album', true);
-      $thumb_url = get_the_post_thumbnail_url($track_id);
+      $media_url = get_post_meta($track_id, '_igv_track_cloudinary', true);
+      if ($media_url) {
+        $related_album = get_post_meta($track_id, '_igv_track_album', true);
+        $thumb_url = get_the_post_thumbnail_url($track_id);
 
-      if (!has_post_thumbnail($track_id) && !empty($related_album)) {
-        $thumb_url = get_the_post_thumbnail_url($related_album, 'thumbnail');
-      }
+        if (!has_post_thumbnail($track_id) && !empty($related_album)) {
+          $thumb_url = get_the_post_thumbnail_url($related_album, 'thumbnail');
+        }
   ?>
     <li class="item-s-12 playlist-item player-skip u-pointer grid-row justify-between align-items-center padding-top-micro padding-bottom-micro flex-nowrap" data-id="<?php echo $track_id; ?>">
       <div class="grid-item">
         <div class="player-thumb-holder">
-          <?php echo $thumb_url ? '<img src="' . $thumb_url . '" class="playlist-item-thumb" alt="' . get_the_title() . ' album cover" data-no-lazysizes="true" />' : ''; ?>
+          <img src="<?php echo $thumb_url ? $thumb_url : ''; ?>" class="playlist-item-thumb <?php echo $thumb_url ? '' : 'u-visuallyhidden'; ?>" alt="<?php echo the_title(); ?> album cover" data-no-lazysizes="true" />
         </div>
       </div>
       <div class="grid-item flex-grow">
@@ -28,7 +30,10 @@ if (!empty($playlist)) {
         </div>
       </div>
     </li>
-  <?php } ?>
+  <?php 
+      }
+    } 
+  ?>
   </ul>
 </div>
 
